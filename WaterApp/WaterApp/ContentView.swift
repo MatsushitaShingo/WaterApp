a09a09832c08bb9f2d24b5d0dcd61668ef264d53
MatsushitaShingo <95203123+MatsushitaShingo@users.noreply.github.com>
2022-01-16 16:40:23 +0900

    initial commit
    練習です

---
+++ WaterApp/WaterApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WaterApp
+//
+//  Created by 松下慎吾 on 2022/01/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("動画")
+            Text("学習")
+            Text("可視化")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
---
+++ WaterApp/WaterApp/WaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterAppApp.swift
+//  WaterApp
+//
+//  Created by 松下慎吾 on 2022/01/16.
+//
+
+import SwiftUI
+
+@main
+struct WaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
